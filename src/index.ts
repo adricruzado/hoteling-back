ad40e807d8fbdd6183c2d06d3f1575b05fbdf66b
Adri Cruzado <adricruzado96@gmail.com>
2023-11-27 18:28:04 +0100

    Add cors configuration

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
+import "dotenv/config";
+import chalk from "chalk";
+import createDebug from "debug";
+import "./server/index.js";
 import { startServer } from "./server/app.js";
+import { connectToDatabase } from "./database/index.js";
+
+const debug = createDebug("hotels:port");
 
 const port = process.env.PORT ?? 4000;
+
+if (!process.env.MONGODB_URL) {
+  debug(chalk.red("Missing MongoDB connection"));
+  process.exit();
+}
+
+const mongoUrl = process.env.MONGODB_URL;
+
+await connectToDatabase(mongoUrl);
 
 startServer(+port);
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,15 @@
-// La cadena de middlewares 📎
+import morgan from "morgan";
+import express from "express";
+import cors from "cors";
+import app from "./app.js";
+
+app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: [
+      "https://adria-cruzado-202309-bcn-back.onrender.com",
+      "https://adria-cruzado-202309-bcn-back.onrender.com/hotels",
+    ],
+  }),
+);
+app.use(express.json());
