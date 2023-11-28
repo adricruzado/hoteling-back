4f6129ba7ed03dcb8a3df7ea11fce2261f9da54e
Adri Cruzado <adricruzado96@gmail.com>
2023-11-28 19:00:33 +0100

    Update tests configuration

--- src/features/hotel/router/getHotelsEndpoint.test.ts
+++ src/features/hotel/router/getHotelsEndpoint.test.ts
@@ -1,26 +1,9 @@
-import { MongoMemoryServer } from "mongodb-memory-server";
 import request from "supertest";
-import { connectToDatabase } from "../../../database";
-import mongoose from "mongoose";
 import app from "../../../server/app";
 import "../../../server/index";
 import Hotel from "../model/Hotel.js";
 import { hotelsMock } from "../mocks/hotelsMock";
 import { type HotelStructure } from "../types";
-
-let server: MongoMemoryServer;
-
-beforeAll(async () => {
-  server = await MongoMemoryServer.create();
-  const mongoDbUrl = server.getUri();
-
-  await connectToDatabase(mongoDbUrl);
-});
-
-afterAll(async () => {
-  await mongoose.disconnect();
-  await server.stop();
-});
 
 describe("Given a GET /hotels endpoint", () => {
   describe("When it receives a request", () => {
