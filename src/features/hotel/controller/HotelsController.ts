d61f62023a24d5f45f396407ef378ae983887789
Adri Cruzado <adricruzado96@gmail.com>
2023-12-04 13:19:32 +0100

    Update HotelsController with deleteHotel method

--- src/features/hotel/controller/HotelsController.ts
+++ src/features/hotel/controller/HotelsController.ts
@@ -1,5 +1,6 @@
-import { type Request, type Response } from "express";
+import { type NextFunction, type Request, type Response } from "express";
 import { type HotelsRepository } from "../repository/types";
+import CustomError from "../../../server/CustomError/CustomError";
 
 class HotelsController {
   constructor(private readonly hotelsRepository: HotelsRepository) {}
@@ -8,6 +9,26 @@ class HotelsController {
     const hotels = await this.hotelsRepository.getHotels();
 
     res.status(200).json({ hotels });
+  };
+
+  deleteHotel = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> => {
+    try {
+      const { hotelId } = req.params;
+
+      await this.hotelsRepository.deleteHotel(hotelId);
+
+      res.status(200).json({});
+    } catch {
+      const error = new CustomError(
+        "An error occurred while deleting the hotel.",
+        500,
+      );
+      next(error);
+    }
   };
 }
 
