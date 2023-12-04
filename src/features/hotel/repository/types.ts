5d806daf136ae7a4618a8c9da3cf73e4bc3e1d34
Adri Cruzado <adricruzado96@gmail.com>
2023-12-04 13:18:09 +0100

    Add deleteHotel method

--- src/features/hotel/repository/types.ts
+++ src/features/hotel/repository/types.ts
@@ -2,4 +2,5 @@ import { type HotelStructure } from "../types";
 
 export interface HotelsRepository {
   getHotels: () => Promise<HotelStructure[]>;
+  deleteHotel: (hotelId: string) => Promise<void>;
 }
