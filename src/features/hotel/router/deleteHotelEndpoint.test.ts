174ca07e4f39b212067613fa644ed5188da0c266
Adri Cruzado <adricruzado96@gmail.com>
2023-12-07 12:21:34 +0100

    Update deleteHotelEndpoint test

--- src/features/hotel/router/deleteHotelEndpoint.test.ts
+++ src/features/hotel/router/deleteHotelEndpoint.test.ts
@@ -1,16 +1,12 @@
 import request from "supertest";
 import app from "../../../server/app";
 import "../../../server/index";
-import Hotel from "../model/Hotel";
-import { hotelsMock } from "../mocks/hotelsMock";
 
-describe("Given a DELETE /hotels/:hotelId endpoint", () => {
-  describe("When it receives a request with a valid id", () => {
+describe("Given a DELETE /hotels/656492250f2c29b159453185 endpoint", () => {
+  describe("When it receives a request with a valid id '656492250f2c29b159453185'", () => {
     test("Then it should respond with status 200 and an empty object", async () => {
       const path = "/hotels/656492250f2c29b159453185";
       const expectedStatusCode = 200;
-
-      await Hotel.deleteOne({ _id: hotelsMock[0]._id });
 
       const response = await request(app)
         .delete(path)
@@ -19,14 +15,15 @@ describe("Given a DELETE /hotels/:hotelId endpoint", () => {
       expect(response.body).toStrictEqual({});
     });
   });
+});
 
-  describe("When it receives a request with an invalid id", () => {
+describe("Given a DELETE /hotels/1234567 endpoint", () => {
+  describe("When it receives a request with an invalid id '1234567'", () => {
     test("Then it should respond with status 500 and a message 'An error occurred while deleting the hotel.'", async () => {
       const path = "/hotels/1234567";
       const expectedStatusCode = 500;
       const expectedErrorMessage =
         "An error occurred while deleting the hotel.";
-      await Hotel.deleteOne({ _id: hotelsMock[0]._id });
 
       const response = await request(app)
         .delete(path)
